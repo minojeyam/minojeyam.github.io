d79adad3c3d96314904bb5e9d618ecae1fe7afeb
M Minerva Jeyarajah <maryannminerva@gmail.com>
2021-05-26 18:03:39 +0530

    Delete Script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(".prod-logo").hover(function () {
-    $(".inner").addClass("inner-hover");
-    $(".nav-li").slideDown(500);
-  },bvhgghjvnjkv
-  function () {
-    $(".inner").removeClass("inner-hover");
-   $(".nav-li").slideUp(500);
-  }
-);
